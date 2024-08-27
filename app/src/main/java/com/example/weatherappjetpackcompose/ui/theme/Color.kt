4204be5038bec9f8e9ccb4921aa0c208a0843b69
coder <soumyodipsutra@gmail.com>
2024-08-27 09:46:38 +0530

    weather app

--- app/src/main/java/com/example/weatherappjetpackcompose/ui/theme/Color.kt
+++ app/src/main/java/com/example/weatherappjetpackcompose/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BlueJc = Color(0xFF3DC6F6)
+val DarkBlueJc = Color(0xFF145DA0)
