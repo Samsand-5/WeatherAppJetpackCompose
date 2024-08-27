7d1479deda3f060ce9cddb1a92be7fd71a5a0da3
coder <soumyodipsutra@gmail.com>
2024-08-27 14:12:26 +0530

    weather app

--- app/src/main/java/com/example/weatherappjetpackcompose/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherappjetpackcompose/WeatherViewModel.kt
@@ -1,11 +1,26 @@
 package com.example.weatherappjetpackcompose
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class WeatherViewModel:ViewModel() {
     private val _weatherData = MutableStateFlow<WeatherResponse?>(null)
     val weatherData : StateFlow<WeatherResponse?> = _weatherData
     private val weatherApi = WeatherApi.create()
+
+    fun fetchWeather(city: String,apiKey: String){
+        viewModelScope.launch {
+            try {
+                val response = weatherApi.getWeather(city, apiKey)
+                _weatherData.value = response
+            }
+            catch (e: Exception){
+
+            }
+        }
+    }
 }
