cf681683bc959ebc42ebd75f282ed911f616b0dd
coder <soumyodipsutra@gmail.com>
2024-08-27 14:12:51 +0530

    weather app

--- app/src/main/java/com/example/weatherappjetpackcompose/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherappjetpackcompose/WeatherViewModel.kt
@@ -19,7 +19,7 @@ class WeatherViewModel:ViewModel() {
                 _weatherData.value = response
             }
             catch (e: Exception){
-
+                e.printStackTrace()
             }
         }
     }
