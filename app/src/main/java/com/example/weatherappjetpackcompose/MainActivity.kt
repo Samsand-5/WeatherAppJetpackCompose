3b09dba4e3d93a4f4102c96d0399e6a1d287e0af
coder <soumyodipsutra@gmail.com>
2024-08-27 14:28:09 +0530

    weather app

--- app/src/main/java/com/example/weatherappjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/weatherappjetpackcompose/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.weatherappjetpackcompose.ui.theme.WeatherAppJetpackComposeTheme
@@ -29,6 +36,18 @@ class MainActivity : ComponentActivity() {
 fun WeatherScreen(){
     val viewModel: WeatherViewModel = viewModel()
     val weatherData by viewModel.weatherData.collectAsState()
+    val city by remember {
+        mutableStateOf("")
+    }
+    val apiKey = "95ea32405d16472f11252e241fb1b891"
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .paint(
+            painterResource(id = R.drawable.ic_launcher_background),
+            contentScale = ContentScale.FillBounds
+        )){
+
+    }
 }
 
 @Preview(showBackground = true)
