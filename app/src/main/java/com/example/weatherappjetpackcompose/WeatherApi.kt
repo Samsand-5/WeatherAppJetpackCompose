dbf79dcc27e2c10ca12c382b5e2d783a83df6501
coder <soumyodipsutra@gmail.com>
2024-08-27 10:57:43 +0530

    weather app

--- app/src/main/java/com/example/weatherappjetpackcompose/WeatherApi.kt
+++ app/src/main/java/com/example/weatherappjetpackcompose/WeatherApi.kt
@@ -10,4 +10,8 @@ interface WeatherApi {
         @Query("appid") apiKey:String,
         @Query("units") units:String ="metric"
     ): WeatherResponse
+
+    companion object{
+        private const val BASE_URL="https://api.openweathermap.org/data/2.5/"
+    }
 }
