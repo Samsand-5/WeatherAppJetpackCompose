80daab3da898de53e3b0b5d6b6b7692db47f1a1d
coder <soumyodipsutra@gmail.com>
2024-08-28 00:24:58 +0530

    designning ui

--- app/src/main/java/com/example/weatherappjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/weatherappjetpackcompose/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -120,7 +121,7 @@ fun weatherCard(label: String, value: String, icon: ImageVector){
                     tint = DarkBlueJc,
                     modifier = Modifier.size(20.dp)
                 )
-
+                Spacer(modifier = Modifier.width(4.dp))
             }
         }
     }
