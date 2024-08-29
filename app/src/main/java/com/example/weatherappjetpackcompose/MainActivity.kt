99f9ada6c9837309d159e4d56ccc42c377c56ab0
coder <soumyodipsutra@gmail.com>
2024-08-29 22:35:05 +0530

    designning ui

--- app/src/main/java/com/example/weatherappjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/weatherappjetpackcompose/MainActivity.kt
@@ -108,7 +108,7 @@ fun WeatherScreen(){
                     WeatherCard(label = "City", value = it.name, icon = Icons.Default.Place)
                     WeatherCard(label = "Temperature", value = "${it.main.temp}", icon = Icons.Default.Star)
                 }
-                Row(modifier = Modifier.fillMaxSize()){
+                Row(modifier = Modifier.fillMaxWidth()){
 
                 }
             }
