a9aa8e60370d611288be5f424721c0b4044e1874
coder <soumyodipsutra@gmail.com>
2024-08-29 22:43:04 +0530

    Completed the app making

--- app/src/main/java/com/example/weatherappjetpackcompose/WeatherResponse.kt
+++ app/src/main/java/com/example/weatherappjetpackcompose/WeatherResponse.kt
@@ -5,10 +5,12 @@ data class WeatherResponse(
     val main:Main,
     val weather:List<Weather>
 )
+
 data class Main(
     val temp:Float,
     val humidity:Int
 )
+
 data class Weather(
     val description:String
 )
