34e1c873557ed84997ac301b45c9e63a7881f790
coder <soumyodipsutra@gmail.com>
2024-08-29 22:42:21 +0530

    designning ui

--- app/src/main/java/com/example/weatherappjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/weatherappjetpackcompose/MainActivity.kt
@@ -57,7 +57,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            WeatherScreen()
         }
     }
 }
